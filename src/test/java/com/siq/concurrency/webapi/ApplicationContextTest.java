9178dea39defd0f480f94d8013730159b2ece922
Tim Myer <tmyer@solutionsiq.com>
2016-08-18 22:29:59 +0530

    Adding failing unit test to verify the lack of thread safety for a lazily-constructed component on a singleton.

--- src/test/java/com/siq/concurrency/webapi/ApplicationContextTest.java
+++ src/test/java/com/siq/concurrency/webapi/ApplicationContextTest.java
@@ -11,6 +11,9 @@ import java.util.stream.IntStream;
 
 import org.junit.Test;
 
+import com.siq.concurrency.webapi.dataaccess.DataStore;
+import com.siq.concurrency.webapi.entities.Region;
+
 public class ApplicationContextTest {
 
     /**
@@ -18,7 +21,7 @@ public class ApplicationContextTest {
      * <strong>Notes:</strong>
      * <p>
      * <ul>
-     * <li>This test will fail sporadically</li>
+     * <li>This test will fail sporadically.</li>
      * <li>This test depends on {@link ApplicationContext#getDefault()} not being called before this test runs.
      * Otherwise this test will not fail.</li>
      * <li>Other unittests that depend on the Application context should instantiate it directly rather than using the
@@ -30,6 +33,27 @@ public class ApplicationContextTest {
         final List<ApplicationContext> collected = IntStream.rangeClosed(1, 10) //
                 .parallel() //
                 .mapToObj(_i -> ApplicationContext.getDefault()) //
+                .collect(toList());
+        assertThat(collected, everyItem(is(sameInstance(collected.get(0)))));
+    }
+
+    /**
+     * <p>
+     * <strong>Notes:</strong>
+     * <p>
+     * <ul>
+     * <li>This test will fail sporadically.</li>
+     * <li>This test creates its own {@link ApplicationContext} and does not really on the default so it should be
+     * runnable in the context of other tests.</li>
+     * </ul>
+     */
+    @Test
+    public void shouldOnlyCreateOneRegionDataStore() {
+        final ApplicationContext applicationContext = new ApplicationContext();
+
+        final List<DataStore<Region>> collected = IntStream.rangeClosed(1, 10) //
+                .parallel() //
+                .mapToObj(_i -> applicationContext.getRegionDataStore()) //
                 .collect(toList());
         assertThat(collected, everyItem(is(sameInstance(collected.get(0)))));
     }
